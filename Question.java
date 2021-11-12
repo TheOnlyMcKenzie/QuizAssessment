d17c29aec81ce3d374f32837f25c408b2df36822
TheOnlyMcKenzie <69824772+TheOnlyMcKenzie@users.noreply.github.com>
2021-11-12 13:17:23 +1300

    Final Version
    Added capability to display images

--- Question.java
+++ Question.java
@@ -1,12 +1,15 @@
 import java.util.HashMap;
 import ecs100.*;
+import java.util.*;
+import java.io.*;
+import java.awt.Color;
 /**
  * Support class for the questions in the quiz
  * An individual question has a number (ID), a statement and a HashMap of answers
  * Will try something to make having multiple answers work in the future
  *
  * @author James McKenzie
- * @version 2.0
+ * @version 4.0 (final version)
  */
 public class Question
 {
@@ -14,23 +17,39 @@ public class Question
     private int questionID;
     private String statement;
     private String answer;
+    private String image;
     private HashMap<Integer, Answer> answers;
     private double locX;
     private double locY;
     private final double POSITION = 200;
-    private final double WIDTH = 100;
-    private final double HEIGHT = 100;
+    private final double WIDTH = 200;
+    private final double HEIGHT = 200;
+    private final double Y = POSITION + HEIGHT;
+    private final double Y2 = Y + 50;
+    private final double Y3 = Y2 + 100;
     private final int FONTSIZE = 16; // Set the font size to 16
+    
+    static final String DEFAULT_IMAGE = "Default_Image.png";
 
     /**
      * Constructor for objects of class Question
      */
-    public Question(int id, String stmnt, HashMap<Integer, Answer> ans)
+    public Question(int id, String stmnt, HashMap<Integer, Answer> ans, String img)
     {
         // initialise instance variables
         this.questionID = id;
         this.statement = stmnt;
         this.answers = ans;
+        this.image = img;
+    }
+    
+    /**
+     * Constructor overload
+     * Set the image to the default image if no image has been entered
+     */
+    public Question(int id, String stmnt, HashMap<Integer, Answer> ans)
+    {
+        this(id, stmnt, ans, DEFAULT_IMAGE);
     }
 
     /**
@@ -63,6 +82,20 @@ public class Question
         return this.answers;
     }
     
+    
+    /**
+     * Display the image in the GUI
+     */
+    public void displayImage()
+    {
+        // Set the position
+        
+        this.locX = POSITION;
+        this.locY = POSITION;
+        
+        // Draw the image
+        UI.drawImage(this.image, this.locX, this.locY, WIDTH, HEIGHT);
+    }
     /**
      * Display the question in the GUI
      */
@@ -71,7 +104,7 @@ public class Question
         // Set the position
         
         this.locX = POSITION;
-        this.locY = POSITION;
+        this.locY = Y;
         
         String question = this.statement;
         
@@ -88,7 +121,7 @@ public class Question
     {
         // Set the position
         this.locX = POSITION;
-        this.locY = POSITION + HEIGHT;
+        this.locY = Y2;
         int counter = 1;
         
         for (int answer : this.answers.keySet())
@@ -100,5 +133,31 @@ public class Question
         }
         
   
+    }
+    
+    /**
+     * Displays if the user's answer was correct or incorrect
+     */
+    public void displayStatus(int number)
+    {
+        // Set the position
+        this.locX = POSITION;
+        this.locY = Y3;
+        String status = "";
+        
+        if (number == 0)
+        {
+            Color col = new Color(0, 153, 15);
+            UI.setColor(col);
+            status = "Correct!";
+        }
+        if (number == 1)
+        {
+            UI.setColor(Color.red);
+            status = "Incorrect!";
+        }
+        
+        UI.setFontSize(FONTSIZE);
+        UI.drawString(status, this.locX, (this.locY + FONTSIZE));
     }
 }
